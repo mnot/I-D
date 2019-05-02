800c201459d2581f8bb2e6dc1d5bb93014275fb2
Mark Nottingham <mnot@mnot.net>
2019-05-02 12:21:00 +1000

    specify loader for yaml

--- Tools/index.py
+++ Tools/index.py
@@ -4,7 +4,7 @@ import sys
 import yaml
 
 try:
-    draft = yaml.load_all(open(sys.argv[1]).read())
+    draft = yaml.load_all(open(sys.argv[1]).read(), Loader=yaml.FullLoader)
 except:
     sys.exit(0)
 for section in draft:
